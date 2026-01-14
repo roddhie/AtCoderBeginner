2a04d547130080a6fcd1bc89b720458c58564706
Roddhie <ts.dwnq@gmail.com>
2026-01-14 22:50:44 +0900

    Add: giveup

---
+++ ABC404/C.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+using namespace std;
+
+// give up 場合分けが複雑になってしまい1時間を超えたためギブアップ
+// int main() {
+//     int T, N, W, sum, ans;
+//     int C[201010];
+//     cin >> T;
+//     rep(i,T) {
+//         cin >> N >> W;
+//         rep(j, N) {
+//             cin >> C[j+1];
+//             sum += C[j+1];
+//         }
+//         int ans_temp;
+//         int brocks = N / (2*W) + 1; 
+//         rep1(x, 2 * W) {
+//             rep1(b, brocks) {
+//                 if(b != brocks)             
+//             }
+//         }
+//     }
+//     return 0;
+// }
