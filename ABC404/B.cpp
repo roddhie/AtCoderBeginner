56ff4583a2b5f4b53a3e3d166d03bc7562dd846d
Roddhie <ts.dwnq@gmail.com>
2026-01-14 21:20:31 +0900

    Add: add AC for lambda expression

--- ABC404/B.cpp
+++ ABC404/B.cpp
@@ -2,20 +2,34 @@
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 
+// vector型、pair型、sort
+// int main() {
+//     int N;
+//     cin >> N;
+//     vector<pair<int, int>> T;
+//     rep(i, N) {
+//         int t;
+//         cin >> t;
+//         pair<int, int> horse(t, i+1);
+//         T.push_back(horse);
+//     }
+
+//     sort(T.begin(), T.end());
+//     rep(i,3) cout << T[i].second << endl;
+
+//     return 0;
+// }
+
+// lambda expression
 int main() {
     int N;
     cin >> N;
-    vector<pair<int, int>> T;
+    vector<int> T(N), t(N);
     rep(i, N) {
-        int t;
-        cin >> t;
-        pair<int, int> horse(t, i+1);
-        T.push_back(horse);
+        cin >> T[i];
+        t[i] = i+1;
     }
-
-    sort(T.begin(), T.end());
-    rep(i,3) cout << T[i].second << endl;
-
+    sort(t.begin(), t.end(), [&](int i, int j) {return T[i] < T[j];});
+    
     return 0;
 }
-// vector型、pair型、sort
