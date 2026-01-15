470cd0b239d4660c7e9a93b027733899d9fa59ce
Roddhie <ts.dwnq@gmail.com>
2026-01-15 23:55:24 +0900

    add: add template with repMacro

---
+++ template/template.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+using namespace std;
+
+int main() {
+    return 0;
+}
