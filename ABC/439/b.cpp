b9121e00b1abb4e25e0e7b4cdec305f61b69675e
Roddhie <ts.dwnq@gmail.com>
2026-06-05 09:27:23 +0900

    Add: 439 B ExampleAnswer

--- ABC/439/b.cpp
+++ ABC/439/b.cpp
@@ -1,38 +1,59 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
 using namespace std;
 
-int calc(int n) {
-    int forth,third,second, first;
-    first   = n%10;
-    second  = n/10%10;
-    third   = n/100%10;
-    forth   = n/1000;
-    return first*first + second*second + third *third + forth*forth;
+// int calc(int n) {
+//     int forth,third,second, first;
+//     first   = n%10;
+//     second  = n/10%10;
+//     third   = n/100%10;
+//     forth   = n/1000;
+//     return first*first + second*second + third *third + forth*forth;
+// }
+
+// int main() {
+//     int N,newN;
+//     cin >> N;
+//     vector<int> T;
+//     T.push_back(0);
+//     bool isHappy = true;
+
+//     while(isHappy == true) {
+//         newN = calc(N);
+//         if (*find(T.begin(), T.end(), newN) == newN){
+//             isHappy = false;
+//         } else if(newN != 1) {
+//             T.push_back(newN);
+//             N = newN;
+//         } else {
+//             break;
+//         }
+//     }
+
+//     string Ans = isHappy ? "Yes" : "No";
+//     cout << Ans << endl;
+    
+//     return 0;
+// }
+
+int f(int n) {
+    int res = 0;
+    while (n ) {
+        int d = n%10;
+        res += d*d;
+        n /= 10;
+    }
+    return res;
 }
 
+// mohan kaito
 int main() {
-    int N,newN;
-    cin >> N;
-    vector<int> T;
-    T.push_back(0);
-    bool isHappy = true;
+    int n;
+    cin >> n;
 
-    while(isHappy == true) {
-        newN = calc(N);
-        if (*find(T.begin(), T.end(), newN) == newN){
-            isHappy = false;
-        } else if(newN != 1) {
-            T.push_back(newN);
-            N = newN;
-        } else {
-            break;
-        }
-    }
+    rep(i, 1000) n = f(n);
 
-    string Ans = isHappy ? "Yes" : "No";
-    cout << Ans << endl;
-    
+    if(n == 1) cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
