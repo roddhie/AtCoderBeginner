1b9ef3c59bde2062989cd3658477f7186db6db7e
Roddhie <ts.dwnq@gmail.com>
2026-09-17 16:20:24 +0900

    add: abc463 test A, B

---
+++ ABC/463/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    if (X * 9 == Y * 16) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
